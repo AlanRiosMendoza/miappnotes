bc3e78ad4144e865d4ce67a83dbfc16b1c72aa5e
Alan Rios <alan.rios.hd@gmail.com>
2024-11-08 15:42:43 -0500

    Agregar módulo de modal y configuración de Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  provideFirebaseApp(() => initializeApp({
+    "projectId": `${environment.firebaseConfig.projectId}`,
+    "appId": `${environment.firebaseConfig.appId}`,
+    "storageBucket": `${environment.firebaseConfig.storageBucket}`,
+    "apiKey": `${environment.firebaseConfig.apiKey}`,
+    "authDomain": `${environment.firebaseConfig.authDomain}`,
+    "messagingSenderId": `${environment.firebaseConfig.messagingSenderId}`,
+    "measurementId": `${environment.firebaseConfig.measurementId}`
+  })),
+  provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBYYBnkiZvkpKHY6gI9S7F0AhZltmprlto",
+    authDomain: "movil-2024b.firebaseapp.com",
+    projectId: "movil-2024b",
+    storageBucket: "movil-2024b.firebasestorage.app",
+    messagingSenderId: "1095177160052",
+    appId: "1:1095177160052:web:6f2cb16ba99e8aec905821",
+    measurementId: "G-473NX1V5MN"
+  }
 };
 
 /*
